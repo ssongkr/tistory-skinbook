6b78ff29ac57ad26f6c75caf86b92c0cba9ac629
jordan.song <jordan.song@kakaocorp.com>
2023-03-11 00:27:51 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../styles/index.scss'
+
+ReactDOM
+  .createRoot(document.getElementById('sb-root') as HTMLElement)
+  .render(<div>티스토리 스킨북</div>)
